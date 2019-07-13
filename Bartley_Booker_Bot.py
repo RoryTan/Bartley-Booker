3b54825ddcefc284c267af9df55a9a279fe7c50c
RoryTan <rory.tan@gmail.com>
2019-07-13 16:36:12 +0800

    Version 2.0
    Navigate straight to Viztec booking and books in loop. Monitor changes to operation using dictionary rather than tuple.

--- Bartley_Booker_Bot.py
+++ Bartley_Booker_Bot.py
@@ -1,71 +1,79 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[9]:
 
 
 from config import keys
-from config import sessions
+from config import slots
+from config import frame
 from selenium import webdriver
 import datetime
 
 
-# In[2]:
+# In[10]:
 
 
-#Get date 2 weeks from now and convert to string
-def get_date():
+#Pick slots
+day_slots = slots[datetime.datetime.today().weekday()]
+
+
+# In[11]:
+
+
+#Get date 2 weeks from now and convert to urlstring
+def get_url(booking_url):
     book_date = datetime.datetime.today() + datetime.timedelta(days=14)
     book_date_str = book_date.strftime('%Y-%m-%d')
-    return book_date_str
+    booking_date_url = booking_url + book_date_str
+    return booking_date_url
 
 #Convert get_date() into xpath
-def booking_xpath(slot): 
-    frame =  str(int(slot[0:2])-5)
-    xpath = '//*[@id="GridTable"]/tbody/tr[2]/td[' + frame +']'
-    return xpath    
+def booking_xpath(day_slot):
+    xpath = '//*[@id="GridTable"]/tbody/tr[2]/td[' + day_slot +']'
+    return xpath
 
-#func to book facility
-def book_facility(k,slot):
+
+# In[12]:
+
+
+def book_facility():
     try:
+        #Go to URl & login
         driver = webdriver.Chrome('./chromedriver_win32/chromedriver.exe')
-        driver.get(k["br_url"])
-        #login
-        driver.find_element_by_xpath('//*[@id="edit-name"]').send_keys(k["user_id"])
-        driver.find_element_by_xpath('//*[@id="edit-pass"]').send_keys(k["password"])
-        driver.find_element_by_xpath('//*[@id="edit-submit"]').click()
-        #Route from BR site to Vitez site
-        driver.find_element_by_xpath('//*[@id="block-block-9"]/div/form/input[2]').click()
-        #Go to booking url and date
-        driver.switch_to.window(driver.window_handles[1])
-        driver.get(k["booking_url"] + get_date())
-        #Book Court - highlight timeslot Frame 2 = 7 am at 1 hour increments
-        driver.find_element_by_xpath(booking_xpath(slot)).click() #day would already have been checked
+        driver.get(get_url(keys['booking_url']))
+        driver.find_element_by_xpath('//*[@id="txtUser"]').send_keys(keys["user_id"]) #User
+        driver.find_element_by_xpath('//*[@id="txtPassword"]').send_keys(keys["password"]) #Password
+        driver.find_element_by_xpath('//*[@id="PageContentArea"]/form[1]/table/tbody/tr/td/table/tbody/tr/td/input[3]').click() #Submit Logon        
+        #Select slots
+        for day_slot in day_slots:
+            try:
+                driver.find_element_by_xpath(booking_xpath(frame[day_slot])).click()
+            except:
+                continue
+        #Submit booking
         driver.find_element_by_xpath('//*[@id="HeaderTable"]/tbody/tr[1]/td/input[2]').click()
-        driver.switch_to.window(driver.window_handles[2])
-        driver.find_element_by_xpath('//*[@id="SubPageContentArea"]/form/table/tbody/tr[5]/td/input').click()
+        driver.switch_to.window(driver.window_handles[1])
+        driver.find_element_by_xpath('//*[@id="SubPageContentArea"]/form/table/tbody/tr[5]/td/input').click()        
         driver.quit()
+        #func to quit
         #write out log file success
         with open('Bartley_Booker_Logs.txt','a+') as f:
-            f.write("Success " + get_date() + " @ " + slot + "\r\n")
+            f.write("Success " + get_url() + " @ " + slot + "\r\n")
     except:
         with open('Bartley_Booker_Logs.txt','a+') as f:
-            f.write("Failure " + get_date() + " @ " + slot + "\r\n")
+            f.write("Failure " + get_url() + " @ " + slot + "\r\n")
 
 
-# In[3]:
-
-
-#Main
-def main(k,slots):
-    for (day,slot) in slots:
-        if datetime.datetime.today().weekday() == day:
-                book_facility(keys,slot)
-
-
-# In[4]:
+# In[13]:
 
 
 if __name__ == '__main__':
-    main(keys,sessions)
+    book_facility()
+
+
+# In[ ]:
+
+
+
 
