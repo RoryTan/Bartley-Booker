0b189728e4fa9215690f21078812bec42ec6f09b
RoryTan <rory.tan@gmail.com>
2019-07-20 15:47:10 +0800

    Fixed some bugs

--- Bartley_Booker_Bot.py
+++ Bartley_Booker_Bot.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
+# In[34]:
 
 
 from config import keys
 from config import slots
 from config import frame
 from selenium import webdriver
+import sys
 import datetime
 
 
-# In[10]:
-
-
-#Pick slots
-day_slots = slots[datetime.datetime.today().weekday()]
-
-
-# In[11]:
+# In[43]:
 
 
 #Get date 2 weeks from now and convert to urlstring
@@ -32,40 +26,50 @@ def get_url(booking_url):
 def booking_xpath(day_slot):
     xpath = '//*[@id="GridTable"]/tbody/tr[2]/td[' + day_slot +']'
     return xpath
+def write_out(message):
+     with open('Bartley_Booker_Logs.txt','a+') as f:
+                f.write(datetime.datetime.today().strftime('%Y-%m-%d:%H:%M') +                          ": "+ message + "\r\n")
 
 
-# In[12]:
+# In[44]:
 
 
 def book_facility():
-    try:
-        #Go to URl & login
-        driver = webdriver.Chrome('./chromedriver_win32/chromedriver.exe')
-        driver.get(get_url(keys['booking_url']))
-        driver.find_element_by_xpath('//*[@id="txtUser"]').send_keys(keys["user_id"]) #User
-        driver.find_element_by_xpath('//*[@id="txtPassword"]').send_keys(keys["password"]) #Password
-        driver.find_element_by_xpath('//*[@id="PageContentArea"]/form[1]/table/tbody/tr/td/table/tbody/tr/td/input[3]').click() #Submit Logon        
-        #Select slots
+    day_slots = slots[datetime.datetime.today().weekday()]        
+    if day_slots:
+        write_out('Found desired booking slots, initating booking')        
+        try:            
+            driver = webdriver.Chrome('./chromedriver_win32/chromedriver.exe')
+            driver.get(get_url(keys['booking_url']))
+            driver.find_element_by_xpath('//*[@id="txtUser"]').send_keys(keys["user_id"]) #User
+            driver.find_element_by_xpath('//*[@id="txtPassword"]').send_keys(keys["password"]) #Password
+            driver.find_element_by_xpath('//*[@id="PageContentArea"]/form[1]/table/tbody/tr/td/table/tbody/tr/td/input[3]').click() #Submit
+        except:
+            write_out("Failed to logon, exiting webdriver")
+            driver.quit()        
+        #Select slots            
         for day_slot in day_slots:
             try:
-                driver.find_element_by_xpath(booking_xpath(frame[day_slot])).click()
-            except:
-                continue
-        #Submit booking
-        driver.find_element_by_xpath('//*[@id="HeaderTable"]/tbody/tr[1]/td/input[2]').click()
-        driver.switch_to.window(driver.window_handles[1])
-        driver.find_element_by_xpath('//*[@id="SubPageContentArea"]/form/table/tbody/tr[5]/td/input').click()        
+                write_out('Trying to book for slot' + frame[day_slot])
+                driver.find_element_by_xpath(booking_xpath(frame[day_slot])).click()                                        
+            except:                    
+                write_out('Failed to book for slot'+ frame[day_slot])
+                continue            
+
+        #Submit booking            
+        try:
+            driver.find_element_by_xpath('//*[@id="HeaderTable"]/tbody/tr[1]/td/input[2]').click()
+            driver.switch_to.window(driver.window_handles[1])
+            driver.find_element_by_xpath('//*[@id="SubPageContentArea"]/form/table/tbody/tr[5]/td/input').click()        
+        except:
+            write_out("No available slots")
+        
         driver.quit()
-        #func to quit
-        #write out log file success
-        with open('Bartley_Booker_Logs.txt','a+') as f:
-            f.write("Success " + get_url() + " @ " + slot + "\r\n")
-    except:
-        with open('Bartley_Booker_Logs.txt','a+') as f:
-            f.write("Failure " + get_url() + " @ " + slot + "\r\n")
+    else:
+        write_out('Found no desired booking slots for ' + datetime.datetime.today().weekday())                
 
 
-# In[13]:
+# In[45]:
 
 
 if __name__ == '__main__':
