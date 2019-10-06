70600ba94cf2cbf396bd897d3f6d8e0712eacead
RoryTan <rory.tan@gmail.com>
2019-10-06 23:49:55 +0800

    Tried code with 3X for loop - seems to work so far.

--- Bartley_Booker_Bot.py
+++ Bartley_Booker_Bot.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 from config import *
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import json
-import sys
 import datetime
 import time
 
 
-# In[10]:
+# In[2]:
 
 
 #Get date 2 weeks from now and convert to urlstring
 def get_url(booking_url):
     book_date = datetime.datetime.today() + datetime.timedelta(days=15)#use 14 when testing 15 when executing
+#     book_date = datetime.datetime.today() + datetime.timedelta(days=14)#use 14 when testing 15 when executing
     book_date_str = book_date.strftime('%Y-%m-%d')
-    print(book_date_str)
     booking_date_url = booking_url + book_date_str
     return booking_date_url
 
@@ -33,9 +38,71 @@ def wait_for_tomorrow():
     wait_time = start - curr
     wait_time_int = wait_time.total_seconds()            
     write_out('Waiting for {}'.format(wait_time_int))
-    time.sleep(wait_time_int)
 
-# booking_verification_report
+
+# In[3]:
+
+
+def book_facility():
+    day_slots = slots[(datetime.datetime.today() + datetime.timedelta(days=1)).weekday()] #use 0 when testing 1 when executing
+#     day_slots = slots[(datetime.datetime.today() + datetime.timedelta(days=0)).weekday()] #use 0 when testing 1 when executing
+
+    if day_slots:
+        write_out('Found desired booking slots, initating booking')
+
+        try:            
+            #Run Headless
+                chrome_options = Options()
+                chrome_options.add_argument("--headless")
+                driver = webdriver.Chrome('./chromedriver_win32/chromedriver.exe',options=chrome_options)
+#             driver = webdriver.Chrome('./chromedriver_win32/chromedriver.exe')
+            driver.get(get_url(keys['booking_url']))
+            driver.find_element_by_xpath('//*[@id="txtUser"]').send_keys(keys["user_id"]) #User
+            driver.find_element_by_xpath('//*[@id="txtPassword"]').send_keys(keys["password"]) #Password
+            driver.find_element_by_xpath('//*[@id="PageContentArea"]/form[1]/table/tbody/tr/td/table/tbody/tr/td/input[3]').click() #Submit
+                wait_for_tomorrow()
+            write_out('Starting...')
+            driver.refresh()            
+        except:
+            driver.quit()
+
+        for i in range(3):
+            try:
+                #refresh at midnight
+                write_out("starting attempt " + str(i+1))
+
+                #Select slots with loop            
+                for day_slot in day_slots:
+                    try:
+                        write_out('Trying to book for slot' + frame[day_slot])
+                        driver.find_element_by_xpath(booking_xpath(frame[day_slot])).click()                                        
+                    except:                    
+                        continue            
+
+                #Attempt to submit booking            
+                try:
+                    driver.find_element_by_xpath('//*[@id="HeaderTable"]/tbody/tr[1]/td/input[2]').click()
+                except:
+                    driver.refresh()
+                    continue
+                try:
+                    driver.switch_to.window(driver.window_handles[1])
+                    driver.find_element_by_xpath('//*[@id="SubPageContentArea"]/form/table/tbody/tr[5]/td/input').click()
+                    break
+                except:
+                    continue                       
+            except ValueError as e:
+                write_out("e")                
+
+        #Exit Chrome Driver
+        driver.quit()
+
+    else:
+        write_out('Found no desired booking slots for ' + datetime.datetime.today().weekday()) 
+
+
+# In[4]:
+
 
 def verify_bookings():
     chrome_options = Options()
@@ -47,8 +114,10 @@ def verify_bookings():
     driver.find_element_by_xpath('//*[@id="txtPassword"]').send_keys(keys["password"]) #Password
     driver.find_element_by_xpath('//*[@id="PageContentArea"]/form[1]/table/tbody/tr/td/table/tbody/tr/td/input[3]').click() #Submit
     
-    book_date = datetime.datetime.today() + datetime.timedelta(days=14) #always use 14
+    book_date = datetime.datetime.today() + datetime.timedelta(days=15)#use 14 when testing 15 when executing
+#     book_date = datetime.datetime.today() + datetime.timedelta(days=14)#use 14 when testing 15 when executing
     book_date_str = book_date.strftime('%d/%m/%Y')
+    
     booking_list = []
     booking_info = {}    
     id_list=[]
@@ -75,57 +144,8 @@ def verify_bookings():
             continue
     driver.quit()
 
-# In[11]:
 
-def book_facility():
-    day_slots = slots[(datetime.datetime.today() + datetime.timedelta(days=1)).weekday()] #use 0 when testing 1 when executing
-    
-    if day_slots:
-        write_out('Found desired booking slots, initating booking')
-        
-        try:            
-            #Run Headless
-            chrome_options = Options()
-            chrome_options.add_argument("--headless")
-            driver = webdriver.Chrome('./chromedriver_win32/chromedriver.exe',options=chrome_options)
-            # driver = webdriver.Chrome('./chromedriver_win32/chromedriver.exe')
-            driver.get(get_url(keys['booking_url']))
-            driver.find_element_by_xpath('//*[@id="txtUser"]').send_keys(keys["user_id"]) #User
-            driver.find_element_by_xpath('//*[@id="txtPassword"]').send_keys(keys["password"]) #Password
-            driver.find_element_by_xpath('//*[@id="PageContentArea"]/form[1]/table/tbody/tr/td/table/tbody/tr/td/input[3]').click() #Submit
-            wait_for_tomorrow()
-            write_out('Starting...')    
-            driver.refresh() 
-
-        except ValueError as e:
-            write_out(e)
-            driver.quit()
-            
-        #Select slots            
-        for day_slot in day_slots:
-            try:
-                write_out('Trying to book for slot' + frame[day_slot])
-                driver.find_element_by_xpath(booking_xpath(frame[day_slot])).click()                                        
-            except:                    
-                write_out('Failed to book for slot'+ frame[day_slot])
-                continue            
-
-        #Submit booking            
-        try:
-            driver.find_element_by_xpath('//*[@id="HeaderTable"]/tbody/tr[1]/td/input[2]').click()
-            driver.switch_to.window(driver.window_handles[1])
-            driver.find_element_by_xpath('//*[@id="SubPageContentArea"]/form/table/tbody/tr[5]/td/input').click()        
-        except:
-            write_out("No available slots") 
-       
-        #Exit Chrome Driver
-        driver.quit()
-        
-    else:
-        write_out('Found no desired booking slots for ' + datetime.datetime.today().weekday())                
-
-
-# In[12]:
+# In[5]:
 
 
 if __name__ == '__main__':
